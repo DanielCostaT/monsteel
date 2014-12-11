da2388c54692d2e0cd1af83561174f3a58b56450
DanielCostaT <danielcosta@toystalk.com>
2014-12-11 16:23:55 -0200

    1st Page Tweens and Audio

---
+++ Assets/Scripts/Misc/TweenAudio.cs
@@ -0,0 +1,26 @@
+﻿// Class to trigger audios through HOTween
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (AudioSource))]
+public class TweenAudio : MonoBehaviour {
+    
+    AudioSource mySource;
+    AudioSource MySource {
+        get {
+            if (mySource == null) {
+                mySource = GetComponent<AudioSource>();
+            }
+            return mySource;
+        }
+    }
+
+    public void Play () {
+        MySource.Play();
+    }
+
+    void Stop () {
+        MySource.Stop();
+    }
+}
