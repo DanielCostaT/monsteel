1f471fe37de830767764c6de7894f2d0836235c3
DanielCostaT <danielcosta@toystalk.com>
2014-12-16 18:45:49 -0200

    First Page Smoke Interaction

---
+++ Assets/Scripts/Misc/TestGroupTweenByID.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+using System.Collections.Generic;
+
+public class TestGroupTweenByID : MonoBehaviour {
+
+    List<IHOTweenComponent> currentAnimation;
+    List<IHOTweenComponent> nextAnimation;
+    List<IHOTweenComponent> allAnimation;
+
+	// Use this for initialization
+    void Start () {
+        allAnimation = HOTween.GetAllTweens();
+        currentAnimation =  HOTween.GetTweensById("P1Q1", true);
+        nextAnimation = HOTween.GetTweensById("P1Q2", true);
+
+        nextAnimation.ForEach(i => i.Play());
+        currentAnimation.ForEach(i => i.Play());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            currentAnimation.ForEach(i => i.Play());
+
+        }
+	}
+
+    void PauseNextTweens () {
+        nextAnimation.ForEach(i => i.Pause());
+    }
+}
---
+++ Assets/Scripts/Misc/TweenInteractionTrigger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using Holoville.HOTween;
+
+public class TweenInteractionTrigger : MonoBehaviour {
+    void OnClick () {
+        HOTween.Play();
+    }
+}
