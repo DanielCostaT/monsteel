7ce8ee025a76a005d96cf9c971fe300c03d3d0ed
DanielCostaT <danielcosta@toystalk.com>
2014-12-18 18:03:10 -0200

    Reset method and Fire Sprite

--- Assets/Scripts/Misc/TestGroupTweenByID.cs
+++ Assets/Scripts/Misc/TestGroupTweenByID.cs
@@ -12,20 +12,27 @@ public class TestGroupTweenByID : MonoBehaviour {
 	// Use this for initialization
     void Start () {
         allAnimation = HOTween.GetAllTweens();
-        currentAnimation =  HOTween.GetTweensById("P1Q1", true);
+        currentAnimation = HOTween.GetTweensById("P1Q1", true);
         nextAnimation = HOTween.GetTweensById("P1Q2", true);
 
-        nextAnimation.ForEach(i => i.Play());
-        currentAnimation.ForEach(i => i.Play());
+        StartPage1();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.A)) {
-            currentAnimation.ForEach(i => i.Play());
-
+        if (Input.touchCount > 2) {
+            if (Input.GetTouch(2).phase == TouchPhase.Began) {
+                StartPage1();
+            }
         }
 	}
+
+    void StartPage1 () {
+        nextAnimation.ForEach(i => i.Rewind());
+        currentAnimation.ForEach(i => i.Rewind());
+        nextAnimation.ForEach(i => i.Play());
+        currentAnimation.ForEach(i => i.Play());
+    }
 
     void PauseNextTweens () {
         nextAnimation.ForEach(i => i.Pause());
